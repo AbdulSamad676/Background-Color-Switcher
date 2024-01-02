2980ee023c773332ba74abe7a92e45db3527a3db
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-02 16:21:40 +0500

    project completed and addtional files added

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,21 @@
-console.log('Welcome to my Project');
+const children = document.querySelectorAll('.child');
+const body = document.querySelector('body');
+
+children.forEach(child => {
+	console.log(child);
+	child.addEventListener('click', e => {
+		if (e.target.id === 'grey') {
+			body.style.backgroundColor = e.target.id;
+		} else if (e.target.id === 'red') {
+			body.style.backgroundColor = e.target.id;
+		} else if (e.target.id === 'yellow') {
+			body.style.backgroundColor = e.target.id;
+		} else if (e.target.id === 'green') {
+			body.style.backgroundColor = e.target.id;
+		} else if (e.target.id === 'pink') {
+			body.style.backgroundColor = e.target.id;
+		} else if (e.target.id === 'purple') {
+			body.style.backgroundColor = e.target.id;
+		}
+	});
+});
